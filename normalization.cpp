039df0dbc11472221093b19b016a5648694031ba
Duc La <nix5005@gmail.com>
2021-04-25 20:48:52 -0700

    Made Normalization function in the cpp file

--- normalization.cpp
+++ normalization.cpp
@@ -3,5 +3,19 @@
 void Normalization::processBuffer(unsigned char* buffer, int bufferSize){
 
 	//process that increases the level of a recording by a constant amount so that it reaches a target or a norm
+	float sumBuffer = 0; 
+	float averageBuffer = 0; 
 
+	//Calculate the average buffer 
+	for (int i = 0; i < bufferSize; i++)
+	{
+		sumBuffer = buffer[i] + sumBuffer;
+		averageBuffer = (sumBuffer/i);  
+	}
+
+	//Adjust the buffers appropriately
+	for (int j = 0; j < bufferSize; j++)
+	{
+		buffer[i] = ((256*.8)/(averageBuffer))*buffer[i]; 
+	}
 }
