c12d0d5e385850a362134324039e9d42e5e61efe
nix505 <60075199+nix505@users.noreply.github.com>
2021-05-04 10:46:27 -0700

    Created Metachunks  struct to store metadata

---
+++ metaChunksHeader.h
@@ -0,0 +1,22 @@
+//CS 202 Semester Project - PT 19
+
+#ifndef METACHUNKS_H
+#define METACHUNKS_H
+#include <vector>
+
+    /**
+     * Struct to contain metadata info management
+     */
+     
+
+typedef struct meta_header {
+    //Not needed for actual data. Just for finding the metadata
+    char list[4]; //LIST
+    int chunkSize; //Chunk Size
+    char info[4]; //INFO
+
+
+  
+} meta_header;
+
+#endif //LISTHEADER_H
