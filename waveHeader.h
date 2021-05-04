d9885ecd95b15d114e8107d7f0e2d8653798cb88
Jordan T Rood <jrood@remote-1.rd.unr.edu>
2021-05-04 12:49:26 -0700

    WaveHeader Doxygen Comments

--- waveHeader.h
+++ waveHeader.h
@@ -8,23 +8,63 @@
 	 */
 typedef struct wav_header {
     // RIFF Header
-    char riff_header[4]; // Contains "RIFF"
-    int wav_size; // Size of the wav portion of the file, which follows the first 8 bytes. File size - 8
-    char wave_header[4]; // Contains "WAVE"
 
-    // Format Header
-    char fmt_header[4]; // Contains "fmt " (includes trailing space)
-    int fmt_chunk_size; // Should be 16 for PCM
-    short audio_format; // Should be 1 for PCM. 3 for IEEE Float
+    /**
+	 * Riff Header variable from wav files, Contains "RIFF"
+	 */
+    char riff_header[4]; 
+	/**
+	 * Size of the wav portion of the file, which follows the first 8 bytes
+	 */
+    int wav_size;
+	/**
+	 * Contains "WAVE" from wav files
+	 */
+    char wave_header[4]; 
+
+    /**
+	 *Format Header - Contains "fmt " (includes trailing space)
+	 */
+    char fmt_header[4]; 
+	/**
+	 * Format chunk size for files, Should be 16 for PCM
+	 */
+    int fmt_chunk_size; 
+	/**
+	 * Audio Format data, Should be 1 for PCM. 3 for IEEE Float
+	 */
+    short audio_format; 
+	/**
+	 * Number of channels in wav files, mono or stereo
+	 */
     short num_channels;
+	/**
+	 * Sample Rate in wav files
+	 */
     int sample_rate;
-    int byte_rate; // Number of bytes per second. sample_rate * num_channels * Bytes Per Sample
-    short sample_alignment; // num_channels * Bytes Per Sample
-    short bit_depth; // Number of bits per sample
+	/**
+	 * Number of bytes per second. sample_rate * num_channels * Bytes Per Sample
+	 */
+    int byte_rate;  
+	/**
+	 * num_channels * Bytes Per Sample
+	 */
+    short sample_alignment;  
+	/**
+	 *  Number of bits per sample from wav files
+	 */
+    short bit_depth;
 
     // Data
-    char data_header[4]; // Contains "data"
-    int data_bytes; // Number of bytes in data. Number of samples * num_channels * sample byte size
+
+	/**
+	 * Contains "data"
+	 */
+    char data_header[4]; 
+	/**
+	 * Number of bytes in data. Number of samples * num_channels * sample byte size
+	 */
+    int data_bytes;
     // char bytes[]; // Remainder of wave file is bytes
 } wav_header;
 
