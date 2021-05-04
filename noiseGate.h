96786fe4e8e4049255ba946791a772e1cdc1f20f
nix505 <60075199+nix505@users.noreply.github.com>
2021-05-03 21:42:07 -0700

    Updated NoiseGate.h for stereo handling

--- noiseGate.h
+++ noiseGate.h
@@ -4,9 +4,6 @@
 #include <iostream>
 #include "processor.h"
 
-/**
- * This is the templated NoiseGate class that inherits from the Processor class.
- */
 
 template<class T>
 class NoiseGate : public Processor<T>
@@ -14,45 +11,59 @@ class NoiseGate : public Processor<T>
 
 	public:
 		int gateLimitPercent; 
-
-	/**
-	 * NoiseGate constructor to obtain gates limit percent
-	 * @param newPercent - percent that the noise amp. will be scaled to, in integers
-	 */
-	NoiseGate(int newPercent): gateLimitPercent(newPercent){}
+		NoiseGate(int newPercent): gateLimitPercent(newPercent){}
         
-	/**
-	 * Overrides abstract class/interface processBuffer to manipulate wav file by eliminating sound between a threshold
-	 * @param buffer - array holding audio buffer, in Templated type T data type
-	 * @param bufferSize - size of wav files buffer, in integers
-	 * @param bitdepth - bit size of wav file, in short integers 
-	 */
-	void processBuffer(T* buffer, int bufferSize, short bitdepth)
-	{
-		int upperThreshold, lowerThreshold; 
-
-		T zeroValue;
-
-		if (bitdepth == 8)
+		void processBuffer(T* buffer, int bufferSize, short bitdepth, short numchannels)
 		{
-			zeroValue = pow(2, bitdepth);
-			upperThreshold = zeroValue + (pow(2, bitdepth)*gateLimitPercent);
-			lowerThreshold = zeroValue - (pow(2, bitdepth)*gateLimitPercent);
-		}
+			int upperThreshold, lowerThreshold; 
 
-		if (bitdepth == 16)
-		{
-			zeroValue = 0;
-			upperThreshold = zeroValue + (pow(2, bitdepth-1)*gateLimitPercent);
-			lowerThreshold = zeroValue + (pow(2, bitdepth-1)*gateLimitPercent);
-		}
+			T zeroValue;
 
-		for(int i = 1; i < bufferSize; i++){
-			if(buffer[i] > upperThreshold && buffer[i] < lowerThreshold){
-				buffer[i] = zeroValue;
+			if (bitdepth == 8)
+			{
+				zeroValue = pow(2, bitdepth);
+				upperThreshold = zeroValue + (pow(2, bitdepth)*gateLimitPercent);
+				lowerThreshold = zeroValue - (pow(2, bitdepth)*gateLimitPercent);
 			}
+
+			if (bitdepth == 16)
+			{
+				zeroValue = 0;
+				upperThreshold = zeroValue + (pow(2, bitdepth-1)*gateLimitPercent);
+				lowerThreshold = zeroValue + (pow(2, bitdepth-1)*gateLimitPercent);
+			}
+
+			if (numchannels == 1)
+			{
+				for(int i = 0; i < bufferSize; i++)
+				{
+					if(buffer[i] < upperThreshold && buffer[i] > lowerThreshold)
+					{
+						buffer[i] = zeroValue;
+					}
+				}
+
+			}
+
+			if (numchannels == 2)
+			{
+				for(int i = 0; i < bufferSize; i+=2){
+					if(buffer[i] < upperThreshold && buffer[i] > lowerThreshold){
+						buffer[i] = zeroValue;
+					}
+				}
+
+				for (int j = 1; j < bufferSize; j+=2)
+				{
+					if (buffer[j] < upperThreshold && buffer[j] > lowerThreshold)
+					{
+						buffer[j] = zeroValue;
+					}
+				}
+			}
+
+
 		}
-	}
 
 };
 
