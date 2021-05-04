58138f2361fc1db032816cb562298eb19f8f6bf8
nix505 <60075199+nix505@users.noreply.github.com>
2021-05-03 21:40:53 -0700

    Updated Echo.h for stereo handling

--- echo.h
+++ echo.h
@@ -4,44 +4,52 @@
 #include "processor.h"
 #include <iostream>
 
-/**
- * This is the templated Echo class that inherits from the Processor class.
- */
 
 template<class T>
 class Echo : public Processor<T>
 {
 	public:
 
-        int delay; 
+                int delay; 
+                Echo(int newDelay): delay(newDelay){}
 
-	/**
-	 * Parameterized Constructor for Echo class that that takes in the delay needed to echo the given wav file
-         * @param newDelay - delay scale, in integers
-	 */
-        Echo(int newDelay): delay(newDelay){}
-	
-	/**
-	 * overrides abstract function processBuffer() and uses delay to make echoed wav file 
-	 * @param buffer - array holding audio buffer, in Templated type T data type
-	 * @param bufferSize - size of wav files buffer, in integers
-	 * @param bitdepth - bit size of wav file, in short integers
-	 */
-	void processBuffer(T* buffer, int bufferSize, short bitdepth)
-        {
-            std::cout << delay << std::endl; 
-            static const T MID = (pow(2, bitdepth)/2);
-            static const float scaleFactor = 0.5f;
-            float echoLevel = 0.5f;
-            float adj_n;
-            float adj_delayed_n;
-            float finalEcho;
 
-                for(int i = 0; i < bufferSize - delay; i++){
-                        adj_n = (float)(buffer[i] - MID);
-                        adj_delayed_n = (float)(buffer[i + delay] - MID);
-                        finalEcho = adj_n * echoLevel + adj_delayed_n * scaleFactor + MID;
-                        buffer[i + delay] = (T)(round(finalEcho));
+
+	void processBuffer(T* buffer, int bufferSize, short bitdepth, short numchannels)
+        {       
+                static const T MID = (pow(2, bitdepth)/2);
+                static const float scaleFactor = 0.5f;
+                float echoLevel = 0.5f;
+                float adj_n;
+                float adj_delayed_n;
+                float finalEcho;
+
+                if (numchannels == 1)
+                {
+                        for(int i = 0; i < bufferSize - delay; i++){
+                                adj_n = (float)(buffer[i] - MID);
+                                adj_delayed_n = (float)(buffer[i + delay] - MID);
+                                finalEcho = adj_n * echoLevel + adj_delayed_n * scaleFactor + MID;
+                                buffer[i + delay] = (T)(round(finalEcho));
+                        }
+                }
+
+                if (numchannels == 2)
+                {
+                        for(int i = 0; i < bufferSize - delay; i+=2){
+                                adj_n = (float)(buffer[i] - MID);
+                                adj_delayed_n = (float)(buffer[i + delay] - MID);
+                                finalEcho = adj_n * echoLevel + adj_delayed_n * scaleFactor + MID;
+                                buffer[i + delay] = (T)(round(finalEcho));
+                        }
+
+                        for (int j = 0; j < bufferSize-delay; j+=2)
+                        {
+                                adj_n = (float)(buffer[j] - MID);
+                                adj_delayed_n = (float)(buffer[j + delay] - MID);
+                                finalEcho = adj_n * echoLevel + adj_delayed_n * scaleFactor + MID;
+                                buffer[j + delay] = (T)(round(finalEcho));
+                        }
                 }
         }
 };
